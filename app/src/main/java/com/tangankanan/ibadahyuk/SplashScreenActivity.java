34f610b1b41bbc12abd5dc84abb51a9058062234
Ibrahim Isbandiputra <ibrahimker@gmail.com>
2015-01-16 22:34:16 +0700

    Ngetest
    Signed-off-by: Ibrahim Isbandiputra <ibrahimker@gmail.com>

---
+++ app/src/main/java/com/tangankanan/ibadahyuk/SplashScreenActivity.java
@@ -0,0 +1,38 @@
+package com.tangankanan.ibadahyuk;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.app.Activity;
+
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        /****** Create Thread that will sleep for 5 seconds *************/
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(1*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(),UserSettingActivity.class);
+                    startActivity(i);
+                    //Remove activity
+                    finish();
+
+                } catch (Exception e) {
+
+                }
+            }
+        };
+
+        // start thread
+        background.start();
+    }
+}
